d9b97e98ccea83ceb74b4fa4ff35ce2196d6cf9e
b3at1 <sambederman@gmail.com>
2023-11-13 20:45:01 -0600

    added basic boundary check

--- fun_game.py
+++ fun_game.py
@@ -1,28 +1,52 @@
 # Title??
 from turtle import *
 
-def functionname(turt:Turtle):
-    turt.forward(10)
+
+######################## PLAYER MOVEMENT ########################
 
 turt = Turtle()
-turt.forward(50)
-turt.right(5)
+turt.speed('fastest')
+turt.penup()
 
-def forward():
+def up():
+    turt.seth(90) # set turtle direction to north
     turt.forward(20)
-def backward():
-    turt.back(20)
-def right():
-    turt.right(20)
-def left():
-    turt.left(20)
 
+def down():
+    turt.seth(270) # set turtle direction to south
+    turt.forward(20)
+def right():
+    turt.seth(0) # set turtle direction to east
+    turt.forward(20)
+def left():
+    turt.seth(180) # set turtle direction to west
+    turt.forward(20)
+def checkbounds():
+    # by default, screen is 960x960.
+    # coords go from (-480,-480) to (480, 480)
+    width = wn.window_width()/2
+    height = wn.window_height()/2
+    if turt.xcor() >= width - 20:
+        turt.setx(width - 20)
+    if turt.ycor() >= height - 20:
+        turt.sety(height - 20)
+    
+    if turt.xcor() <= -1 * width + 20:
+        turt.setx( -1 * width + 20)    
+    if turt.ycor() <= -1 * height + 20:
+        turt.sety(-1 * height + 20)
+    wn.ontimer(checkbounds, 20)
 getscreen()
 wn = Screen()
 wn.listen()
-wn.onkey(forward, key="w")
-wn.onkey(backward, key="a")
-wn.onkey(right, key="s")
-wn.onkey(left, key="d")
+checkbounds()
+wn.onkeypress(up, key="w")
+wn.onkeypress(left, key="a")
+wn.onkeypress(down, key="s")
+wn.onkeypress(right, key="d")
+
+
+
+
 
 wn.exitonclick()
