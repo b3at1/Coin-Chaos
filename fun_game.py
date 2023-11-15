34cb51ff9b592da0104f4f88447f1e13bd38d890
b3at1 <sambederman@gmail.com>
2023-11-14 19:30:16 -0600

    added enemy_max

--- fun_game.py
+++ fun_game.py
@@ -6,31 +6,36 @@ import random
 wn = Screen()
 width = wn.window_width()/2
 height = wn.window_height()/2
-######################## BOT MOVEMENT ########################
+
+######################## ENEMY LOGIC ########################
 
 # TODO: make spawn_enemy method
 start = time.time()
 end = start
 print(start)
-
+enemy_count = 0
+enemy_max = 5 # keep max enemys to 5, for now
 def move_enemy(enemy):
         enemy.pendown()
         enemy.showturtle()
         enemy.goto(random.randint(-1 * width + 20, width - 20), random.randint(-1 * height + 20, height - 20))
         # CALL COLLISION LISTENER HERE
-        wn.ontimer(lambda: move_enemy(enemy) , 100)
-def spawn_enemy():
-        enemy = Turtle(visible=False)
-        enemy.speed('fastest')
-        enemy.penup()
-        enemy.goto(random.randint(-1 * width + 20, width - 20), random.randint(-1 * height + 20, height - 20))
-        move_enemy(enemy)
+        wn.ontimer(lambda: move_enemy(enemy) , 100) # kinda breaks code on exit but its fine.
+
+def spawn_enemy(enemy_count, enemy_max):
+        if enemy_count < enemy_max:
+            enemy_count += 1
+            enemy = Turtle(visible=False)
+            enemy.speed('fastest')
+            enemy.penup()
+            enemy.goto(random.randint(-1 * width + 20, width - 20), random.randint(-1 * height + 20, height - 20))
+            move_enemy(enemy)
         # CALL COLLISION LISTENER HERE
-        wn.ontimer(spawn_enemy, 1000)
+        wn.ontimer(lambda: spawn_enemy(enemy_count, enemy_max), 1000)
         #print(1000 - int(time.time() - start))
         #wn.ontimer(spawn_enemy, 1000 - 2 * int(time.time() - start)) # use current time to speed up the spawning of enemy?
 
-######################## PLAYER MOVEMENT ########################
+######################## PLAYER LOGIC ########################
 
 turt = Turtle()
 turt.speed('fastest')
@@ -67,7 +72,7 @@ wn.bgcolor('red')
 wn.listen()
 
 checkbounds()
-spawn_enemy()
+spawn_enemy(enemy_count, enemy_max)
 
 wn.onkeypress(up, key="w")
 wn.onkeypress(left, key="a")
